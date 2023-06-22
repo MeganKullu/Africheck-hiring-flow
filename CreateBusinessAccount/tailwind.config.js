31f9857277be37db0782e16b6dc137f7e4aae97e
MeganKullu <kullumegan@gmail.com>
2023-06-22 17:11:33 +0300

    mobile and desktop create business account done

---
+++ CreateBusinessAccount/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: '#007DFF',
+        'light-blue':'#D1E8FF'
+      }
+    },
+  },
+  plugins: [],
+}
+
