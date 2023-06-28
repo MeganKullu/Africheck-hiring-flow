7902e46b83b6822e3d0f0e04981a950714c4c041
MeganKullu <kullumegan@gmail.com>
2023-06-28 22:19:34 +0300

    other category responsive design

---
+++ OtherCategory/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: '#007DFF',
+        'light-blue':'#D1E8FF',
+        'dark-blue':'#001634',
+      }
+    },
+  },
+  plugins: [],
+}
+
